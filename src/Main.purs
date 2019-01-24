7ae1f553698c7015f91da35c58cbc649010ba5e9
Joel McCracken <mccracken.joel@gmail.com>
2019-01-24 00:29:02 -0500

    compiling using just the ps-react stuff, no ps react native

--- src/Main.purs
+++ src/Main.purs
@@ -3,38 +3,52 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import React (ReactClass, statelessComponent, component, createLeafElement, ReactElement)
+import React (ReactClass, statelessComponent, component, createLeafElement, ReactElement, getState)
 import ReactNative.API (registerComponent)
 import ReactNative.Components.Text (text_)
 import ReactNative.Components.View (view, view_)
 import ReactNative.Components.Button (button_)
+import ReactNative.Components.ListView (listView, listViewDataSource, ListViewDataSource)
 import ReactNative.Styles (Styles, staticStyles, marginTop)
 -- import Dispatcher.React (getProps, getState, modifyState, renderer, saveRef, withRef)
-import Dispatcher.React (renderer)
+import Dispatcher.React (renderer -- , getState
+                        )
+-- import Debug.Trace
 
 itemsListClass :: ReactClass {}
 itemsListClass = component "ItemsList" spec
   where
     spec this = do
-      let render _ =
-            view_
+      pure { render: render <$> getState this -- renderer render this
+           , state: initialState}
+      where
+        -- foo = spy "this = " this
+        render state =
+          let
+            ds = (listViewDataSource [])
+          -- state <- getState this'
+          in
+          view_
             [ button_ "A Button"
+            , listView state.dataSource rowRender
             , text_ "Hello again"
             ]
-
-      pure {render: renderer render this , state: initialState}
+        rowRender x = text_ "item"
+      -- pure {render: renderer render this, state: initialState}
 
 type ItemsListProps = {}
 
 type State =
   { nextId :: Int
   , items :: Array String
+  , dataSource :: ListViewDataSource String
   }
 
 initialState :: State
 initialState =
   { nextId: 0
   , items: []
+  , dataSource: listViewDataSource []
   }
 
 itemsList :: ItemsListProps -> ReactElement
