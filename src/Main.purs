583aba13c6dc231801dbbd6f3e3c992b9d11eeef
Joel McCracken <mccracken.joel@gmail.com>
2019-01-23 23:45:46 -0500

    some stuff working

--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,54 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import React (ReactClass, statelessComponent)
+import React (ReactClass, statelessComponent, component, createLeafElement, ReactElement)
 import ReactNative.API (registerComponent)
 import ReactNative.Components.Text (text_)
+import ReactNative.Components.View (view)
+import ReactNative.Styles (Styles, staticStyles, marginTop)
+-- import Dispatcher.React (getProps, getState, modifyState, renderer, saveRef, withRef)
+import Dispatcher.React (renderer)
 
-app :: forall p. ReactClass {|p}
+itemsListClass :: ReactClass {}
+itemsListClass = component "ItemsList" spec
+  where
+    spec this = do
+      let render _ = text_ "Hello again"
+      pure {render: renderer render this , state: initialState}
+
+type ItemsListProps = {}
+
+type State =
+  { nextId :: Int
+  , items :: Array String
+  }
+
+initialState :: State
+initialState =
+  { nextId: 0
+  , items: []
+  }
+
+itemsList :: ItemsListProps -> ReactElement
+itemsList = createLeafElement itemsListClass
+
+app :: ReactClass {}
 app = statelessComponent render
   where
-    render _ = text_ "Hello World"
+    render _ = view sheet.container
+               [ text_ "This is a list!"
+               , itemsList {}
+               ]
+
+sheet :: {
+    container :: Styles
+}
+sheet = {
+  container: staticStyles [
+      marginTop 64
+  ]
+}
+
 
 main :: Effect Unit
 main = do
