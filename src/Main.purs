dc95d902dff83ecbcf446dd5269faf5fbadca63d
Joel McCracken <mccracken.joel@gmail.com>
2019-02-13 22:24:51 -0500

    start showing text state

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,14 @@ module Main where
 import Prelude (($), (+), (<>), show)
 -- import Effect.Console
 -- import Effect
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
+import Data.Nullable (toMaybe)
 import React.Basic (StateUpdate(..), JSX, make, runUpdate, Component, createComponent)
-import React.Basic.Native (text_, string, view_, button)
-import React.Basic.DOM.Events (capture_)
--- import Unsafe.Coerce (unsafeCoerce)
+import React.Basic.Native (text_, text, string, view_, button, view, textInput)
+import React.Basic.Events (EventFn, SyntheticEvent, unsafeEventFn, merge)
+import React.Basic.DOM (css)
+import React.Basic.DOM.Events (capture_, capture)
+import Unsafe.Coerce (unsafeCoerce)
 
 comp :: Component {}
 comp = createComponent "Main"
@@ -13,12 +17,19 @@ comp = createComponent "Main"
 data Action
   = Increment
 
+type State =
+  { count :: Int
+  , textVal :: Maybe String
+  }
+initialVal :: State
+initialVal = { count: 0, textVal: Nothing }
+
 -- main :: RB.JSX
 -- main = RN.text_ [ RN.string "hey" ]
 
 main :: JSX
 main = make comp
-  { render, initialState: {count: 0} } {}
+  { render, initialState: initialVal } {}
   where
     update self =
       case _ of
@@ -28,10 +39,27 @@ main = make comp
     send = runUpdate update
 
     -- render :: Self {} State Action -> JSX
-    render self =
-      view_
-        [ text_ [ string $ "hey " <> show self.state.count ]
-        , button { title: "clicky"
-                 , onPress: (capture_ $ send self Increment )
-                 }
-        ]
+    render self = -- text_ [ string $ "hi"]
+      view { style: css {flexDirection: "column", padding: 100}
+           , children:
+             [ text {  key: "foo",
+                       children: [
+                         string $ "hey " <> show self.state.count <> ";" <> show self.state.textVal ]}
+             , textInput { key: "txtinput"
+                         , placeholder: "type here"
+                         , style: css { flex: 1 }
+                         , onChange: (capture (merge {getText}) setThing)
+                         }
+             , button { title: "clickx"
+                      , key: "clickyButton"
+                      , onPress: (capture_ $ send self Increment)
+                      }
+             ]
+           }
+        where
+          setThing event =
+            self.setState _ { textVal = event.getText }
+
+getText :: EventFn SyntheticEvent (Maybe String)
+getText = unsafeEventFn \e ->
+  toMaybe (unsafeCoerce e).nativeEvent.text
