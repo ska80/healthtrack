679be10205ce9ce3a8ab8fcf137d62dea718d13d
Joel McCracken <mccracken.joel@gmail.com>
2019-02-13 23:45:39 -0500

    saving list of items

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,11 @@
 module Main where
-import Prelude (($), (+), (<>), show)
+import Prelude (($), (+), (<>), show, identity)
 -- import Effect.Console
 -- import Effect
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Nullable (toMaybe)
-import React.Basic (StateUpdate(..), JSX, make, runUpdate, Component, createComponent)
+import React.Basic (Self, StateUpdate(..), JSX, make, runUpdate, Component, createComponent)
 import React.Basic.Native (text_, text, string, view_, button, view, textInput)
 import React.Basic.Events (EventFn, SyntheticEvent, unsafeEventFn, merge)
 import React.Basic.DOM (css)
@@ -15,14 +16,18 @@ comp :: Component {}
 comp = createComponent "Main"
 
 data Action
-  = Increment
+  = AddItem
 
 type State =
-  { count :: Int
+  { nextId :: Int
   , textVal :: Maybe String
+  , items :: List String
   }
+
 initialVal :: State
-initialVal = { count: 0, textVal: Nothing }
+initialVal = { nextId: 0,
+               textVal: Nothing,
+               items: Nil }
 
 -- main :: RB.JSX
 -- main = RN.text_ [ RN.string "hey" ]
@@ -33,32 +38,57 @@ main = make comp
   where
     update self =
       case _ of
-        Increment ->
-          Update $ self.state { count = self.state.count + 1 }
+        AddItem ->
+          let
+            nextEntry :: String
+            nextEntry = show self.state.nextId <> ": " <> maybe "" identity self.state.textVal
+          in
+           Update $ self.state {
+             items =  nextEntry : self.state.items,
+             nextId = self.state.nextId + 1,
+             textVal = Nothing
+             }
 
     send = runUpdate update
 
-    -- render :: Self {} State Action -> JSX
-    render self = -- text_ [ string $ "hi"]
+    render self =
       view { style: css {flexDirection: "column", padding: 100}
            , children:
-             [ text {  key: "foo",
-                       children: [
-                         string $ "hey " <> show self.state.count <> ";" <> show self.state.textVal ]}
+             [ debugLabelInfoView self
              , textInput { key: "txtinput"
                          , placeholder: "type here"
                          , style: css { flex: 1 }
-                         , onChange: (capture (merge {getText}) setThing)
+                         , onChange: (capture getText setStateText)
+                         , value: maybe "" identity self.state.textVal
+                         , onSubmitEditing: (capture_ $ send self AddItem )
                          }
              , button { title: "clickx"
                       , key: "clickyButton"
-                      , onPress: (capture_ $ send self Increment)
+                      , onPress: (capture_ $ send self AddItem )
                       }
              ]
            }
         where
-          setThing event =
-            self.setState _ { textVal = event.getText }
+          setStateText event =
+            self.setState _ { textVal = event}
+
+showDebugInfo :: Boolean
+-- showDebugInfo = false
+showDebugInfo = true
+
+debugLabelInfoView :: Self {} State -> JSX
+debugLabelInfoView self =
+  if showDebugInfo then
+    let
+      debugMsg = "debug: " <> show self.state.nextId <>
+                 ";" <> show self.state.textVal <>
+                 ";" <> show self.state.items
+    in
+     text {  key: "debugLabelInfo",
+             children: [string debugMsg]
+          }
+  else
+    view {key: "debugLabelInfoEmpty", children: []}
 
 getText :: EventFn SyntheticEvent (Maybe String)
 getText = unsafeEventFn \e ->
